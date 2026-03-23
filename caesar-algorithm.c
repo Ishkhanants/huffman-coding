af3e72c7eda93032c4c7a451d6826b57819b5909
Martin <martinmirzoyan00@gmail.com>
2026-03-23 23:48:42 +0400

    Added Caesar's cipher

---
+++ caesar-algorithm.c
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include <iostream>
+
+int main()
+//Caesar Aiboubinne #2
+{
+	int i, n, x;
+	char str[100];
+        
+        printf("\nMutqagrvatz:\t");
+	gets_s(str);
+	
+        printf("Banali:"); scanf_s("%d", &n);
+		for (i = 0; (i < 100 && str[i] != '\0'); i++)
+			str[i] = str[i] + n; 
+        
+        printf("\nVerapohvatz:%s\n", str);
+}
